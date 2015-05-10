e05b7082eb7ee4a4ce245041fc53e2effb1db1a1
vinh <vinhtt0305@gmail.com>
2015-05-10 14:55:11 +0700

    warrior lvl 1-8

--- warrior.rb
+++ warrior.rb
@@ -1,22 +1,30 @@
-class Player
 
+class Player
   def initialize
     @health = 20
     @turn = 1
+    @already_in_first_step = false
   end
-
+  
   def play_turn(warrior)
-    if at_first_step_in_first_turn?(warrior)
+    if warrior.feel.wall?
+      warrior.pivot!
+      @turn -= 1
+    elsif at_first_step_in_first_turn?(warrior) || @already_in_first_step
       go_forward(warrior)
-    else
+    elsif !@already_in_first_step
       go_backward(warrior)
     end
     @health = warrior.health
-    @turn ++
+    @turn += 1
   end
-
+  
   def go_forward warrior
-    if !is_attacked?(warrior.health) && need_health?(warrior.health) && warrior.feel.empty? 
+    if has_enemy_in_three_step?(warrior.look)
+      warrior.shoot!
+    elsif is_attacked?(warrior.health) && need_back?(warrior.health)
+      warrior.walk!(:backward)
+    elsif !is_attacked?(warrior.health) && need_health?(warrior.health) && warrior.feel.empty?
       warrior.rest!
     elsif warrior.feel.empty?
         warrior.walk!
@@ -26,26 +34,45 @@ class Player
       warrior.attack!
     end
   end
-
+  
   def go_backward warrior
     if warrior.feel(:backward).empty?
       warrior.walk!(:backward)
     elsif warrior.feel(:backward).captive?
       warrior.rescue!(:backward)
     elsif warrior.feel(:backward).wall?
+      @already_in_first_step = true
       go_forward warrior
     end
   end
-
+  
   def need_health? health
     health < 20
   end
-
+  
   def is_attacked? health
     health < @health
   end
-
+  
   def at_first_step_in_first_turn? warrior
-    @turn == 1 && warrior.feel(:backward).wall?
+    if @turn == 1 && warrior.feel(:backward).wall?
+      @already_in_first_step = true
+      return true
+    else
+      return false
+    end
+  end
+
+  def need_back? health
+    health < 10
+  end
+
+  def has_enemy_in_three_step? spaces
+    spaces.each do |space|
+      return false if space.captive?
+      return true if space.enemy?
+    end
+    false
   end
 end
+  
